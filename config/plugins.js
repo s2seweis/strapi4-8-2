03ae628b1436cb47ed2a5d8c87b4ccadf778690a
weissenborn24seb@gmail.com <weissenborn24seb@gmail.com>
2023-05-10 20:13:10 +0200

    upload,

--- config/plugins.js
+++ config/plugins.js
@@ -1,19 +1,53 @@
 
-module.exports = ({ env }) => ({
-    // ...
+// module.exports = ({ env }) => ({
+//     // ...
+//     upload: {
+//       config: {
+//         provider: 'aws-s3', // For community providers pass the full package name (e.g. provider: 'strapi-provider-upload-google-cloud-storage')
+//         providerOptions: {
+//           accessKeyId: env('AWS_ACCESS_KEY_ID'),
+//           secretAccessKey: env('AWS_ACCESS_SECRET'),
+//           region: 'eu-central-1',
+//           params: {
+//             ACL: env('AWS_ACL', 'public-read'), // 'private' if you want to make the uploaded files private
+//             Bucket: 'strapiuploaddemo996',
+//           },
+//         },
+//       },
+//     },
+//     // ...
+//   });
+
+module.exports = ({ env })=>({
     upload: {
-      config: {
-        provider: 'aws-s3', // For community providers pass the full package name (e.g. provider: 'strapi-provider-upload-google-cloud-storage')
+      providerOptions: {
+        localServer: {
+          maxage: 300000
+        }
+      }
+    }
+  });
+  
+  
+  
+  
+  
+  
+  
+  
+    module.exports = ({ env }) => ({
+      upload: {
+        provider: 'aws-s3',
+  
         providerOptions: {
           accessKeyId: env('AWS_ACCESS_KEY_ID'),
           secretAccessKey: env('AWS_ACCESS_SECRET'),
           region: 'eu-central-1',
           params: {
-            ACL: env('AWS_ACL', 'public-read'), // 'private' if you want to make the uploaded files private
             Bucket: 'strapiuploaddemo996',
           },
         },
       },
-    },
-    // ...
-  });
+    });
+  
+  
