08898fcbaf54a2911f542201626420f53ab98d53
weissenborn24seb@gmail.com <weissenborn24seb@gmail.com>
2023-05-22 00:08:40 +0200

    add e3

---
+++ src/api/content3/controllers/content3.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * content3 controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::content3.content3');
---
+++ src/api/content3/services/content3.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * content3 service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::content3.content3');
